d6cfea04f79fd0183c3be34303ba8486b91bc423
Lucas Eduardo <leduardo.santos.1997@gmail.com>
2022-02-16 23:03:34 -0300

    final version of calculator

--- script.js
+++ script.js
@@ -4,50 +4,88 @@ const display = document.querySelector('.calculator--display p')
 let allowsToReplace = true
 let calc = ''
 
-function handleNumber(value) {
+const handleNumber = value => {
   if (display.textContent.length >= 11) {
     return
   }
-  
+
   if (display.textContent === '0' || allowsToReplace) {
     display.textContent = value
-    
+
     allowsToReplace = false
   } else {
     display.textContent += value
   }
 }
 
-function handleClear() {
+const handleClear = () => {
   display.textContent = '0'
   resume.textContent = ''
   allowsToReplace = true
   calc = ''
 }
 
-function backspace() {
+const backspace = () => {
   const total = display.textContent.length
   const newValue = display.textContent.substring(0, total - 1)
-  
+
   display.textContent = newValue || '0'
 }
 
-function handleAction(action) {
+const handleAction = action => {
   if (action !== '=') {
     const textResume = `${display.textContent}${action}`
-    
+
     resume.textContent += textResume
-    
+
     calc += textResume
   } else {
-     resume.textContent = ''
-    
+    resume.textContent = ''
+
     calc += display.textContent
-    
+
     display.textContent = eval(calc)
-    
+
     calc = ''
   }
-  
+
   allowsToReplace = true
 }
+
+const effectHover = action => {
+  const element = document.getElementById(action)
+
+  element.style.filter = 'brightness(0.8)'
+
+  setTimeout(() => element.style.filter = null, 200)
+}
+
+document.addEventListener('keyup', (event) => {
+  if (event.key === 'Escape' || event.key === 'Delete') {
+    handleClear()
+
+    effectHover('clear')
+  } else if (event.key === 'Backspace') {
+    backspace()
+
+    effectHover('back')
+  }
+})
+
+document.addEventListener('keypress', (event) => {
+  const convertStringToNumber = Number(event.key)
+
+  if (convertStringToNumber >= 0) {
+    handleNumber(convertStringToNumber)
+
+    effectHover(event.key)
+  } else if (event.key !== 'Enter') {
+    handleAction(event.key)
+
+    effectHover(event.key)
+  } else if (event.key === 'Enter' || event.key === '=') {
+    handleAction('=')
+
+    effectHover('=')
+  }
+})
